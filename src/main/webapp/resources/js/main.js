3f8f56526c74723463fe8cb16804362e3b9151ba
LYB <naincon1@gmail.com>
2020-02-22 12:46:53 +0900

    20200222 12:46

---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp',[]);
+app.controller('myCtrl', function($scope, $http, $location) {
+	
+	$scope.memberList = function(){
+		console.log("AAAAA");
+		
+
+		
+	}
+	
+}); 
+	
+	
---
+++ src/main/webapp/resources/js/memberList.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp',[]);
+app.controller('memberList',function($scope){
+	
+	
+	console.log("@@@");
+	console.log($scope.memberList);
+	
+	
+})
